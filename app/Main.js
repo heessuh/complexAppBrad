9eca8d316ab13e8c4c340b88fa9d59233ea9c610
Fred Suh <fredsuh88@gmail.com>
2022-01-26 11:32:38 -0300

    Initial commit

---
+++ app/Main.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// My Components
+import Header from './components/Header'
+import HomeGuest from './components/HomeGuest'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import About from './components/About'
+import Terms from './components/Terms'
+
+function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexAppToken')))
+  return (
+    <BrowserRouter>
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Routes>
+        <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<Main />, document.querySelector('#app'))
+
+if (module.hot) {
+  module.hot.accept()
+}
